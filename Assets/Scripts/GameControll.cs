58eab3a166be6e3d662311b73545fb59d8aa7302
Hoeun <37548919+leehe228@users.noreply.github.com>
2021-01-06 23:09:58 +0900

    initializing

--- Assets/Scripts/GameControll.cs
+++ Assets/Scripts/GameControll.cs
@@ -4,36 +4,15 @@ using UnityEngine;
 
 public class GameControll : MonoBehaviour
 {
-    public bool checkStateFlag;
-
-    public List<Tile> tileList = new List<Tile>();
-
+    // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < 3; i++)
-        {
-            for (int j = 0; j < 4; j++)
-            {
-                Tile tempTile = GameObject.Find("tile" + i.ToString() + j.ToString()).GetComponent<Tile>();
-                tileList.Add(tempTile);
-            }
-        }
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
-
-    public void SetHighLight(string animal, int x, int y)
-    {
-        switch (animal)
-        {
-            case "chick":
-                {
-
-                }
-        }
     }
 }
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -4,22 +4,15 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
-    SpriteRenderer renderer;
-
     // Start is called before the first frame update
     void Start()
     {
-        renderer = GetComponent<SpriteRenderer>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
-
-    public void HighLight()
-    {
-        renderer.color = new Color(255f/255f, 136f/255f, 136f/255f, 125f/255f);
     }
 }
